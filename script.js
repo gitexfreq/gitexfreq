f5f23f4b6521aa059e637e59b833b038cb0836e6
gitexfreq <142749631+gitexfreq@users.noreply.github.com>
2026-08-07 09:16:43 -0700

    Remove mobile SVG letterboxing

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const background = document.getElementById('background');
 
 function animateBackground() {
     const gradient = `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 600">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 600" preserveAspectRatio="none">
             <defs>
                 <radialGradient id="psychedelic-gradient" cx="50%" cy="50%" r="${50 + 10 * Math.sin(Date.now() / 2000)}%">
                     <stop offset="0%" style="stop-color:gold;" />
